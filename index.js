05e02d3eb5576ad8903784ee1b67cb90a0b3146a
Hlib Astashev <Astashev01@gmail.com>
2024-04-06 12:31:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/* console.log('Hello!')
+
+window.alert('Alert') */
+
+/* document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza" */
+
+//------LEVEL 1----------------------------------------------------------
+
+/* let number = 10; 
+
+console.log(`X is: ${number}`); 
+
+let name = "Gleb";
+let age = 22;
+let isStudent = true;
+
+document.getElementById("myP1").textContent = `Your name is ${name}`;
+document.getElementById("myP2").textContent = `Your age is ${age}`;
+document.getElementById("myP3").textContent = `You are student: ${isStudent}`;
+ */
+//------LEVEL 2-----------------------------------------------------------
+
+
+
